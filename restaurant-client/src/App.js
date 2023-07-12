133916490cb7058574469f3f4454de05f3aaadb9
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-07-12 18:57:52 -0300

    Creacion de rutas y componentes

--- restaurant-client/src/App.js
+++ restaurant-client/src/App.js
@@ -1,12 +1,16 @@
 import { Routes, Route } from 'react-router';
 
 import Ordenes from './components/paginas/Ordenes';
+import Menu from './components/paginas/Menu';
+import NuevoPlato from './components/paginas/NuevoPlato';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={ <Ordenes /> } />
+        <Route path="/menu" element={ <Menu /> } />
+        <Route path="/nuevo-plato" element={ <NuevoPlato /> } />
       </Routes>
     </div>
   );
---
+++ restaurant-client/src/components/paginas/NuevoPlato.js
@@ -0,0 +1,9 @@
+const NuevoPlato = () => {
+    return (
+      <>
+        <h1 className="text-3xl font-light mb-4">NuevoPlato</h1>
+      </>
+    )
+  }
+  
+  export default NuevoPlato;
