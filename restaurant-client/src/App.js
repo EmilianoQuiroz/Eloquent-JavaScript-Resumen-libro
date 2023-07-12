9c3e34fd1412fe4ad04fde6e0e08932380c911b8
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-07-12 19:29:37 -0300

    Routing para nuevos platos

--- restaurant-client/src/App.js
+++ restaurant-client/src/App.js
@@ -1,20 +1,19 @@
-import { Routes, Route } from 'react-router';
-import Ordenes from './components/paginas/Ordenes';
-import Menu from './components/paginas/Menu';
-import NuevoPlato from './components/paginas/NuevoPlato';
-import Sidebar from './components/ui/Sidebar';
-
+import { Routes, Route } from "react-router";
+import Ordenes from "./components/paginas/Ordenes";
+import Menu from "./components/paginas/Menu";
+import NuevoPlato from "./components/paginas/NuevoPlato";
+import Sidebar from "./components/ui/Sidebar";
 
 function App() {
   return (
-    <div className='md:flex min-h-screen'>
-    <Sidebar />
-      <div className='md:w-3/5 xl:w-4/5'>
-      <Routes>
-        <Route path="/" element={ <Ordenes /> } />
-        <Route path="/menu" element={ <Menu /> } />
-        <Route path="/nuevo-plato" element={ <NuevoPlato /> } />
-      </Routes>
+    <div className="md:flex min-h-screen">
+      <Sidebar />
+      <div className="md:w-3/5 xl:w-4/5 p-6">
+        <Routes>
+          <Route path="/" element={<Ordenes />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/nuevo-plato" element={<NuevoPlato />} />
+        </Routes>
       </div>
     </div>
   );
