9338eb61f9a1fb2398f5e8760814c901f52b840e
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-07-26 19:55:09 -0300

    Acceder a los datos

--- restaurant-client/src/components/paginas/Menu.js
+++ restaurant-client/src/components/paginas/Menu.js
@@ -1,6 +1,24 @@
+import React, { useState, useEffect, useContext } from 'react';
 import { Link } from "react-router-dom";
+import { FirebaseContext } from '../../firebase';
 
 const Menu = () => {
+
+const { firebase } = useContext(FirebaseContext);
+  
+useEffect(() => {
+  const obtenerPlatos =  () => {
+    const resultado =  firebase.db.collection('productos').onSnapshot(manejarSnapshot);
+
+  }
+  obtenerPlatos();
+}, []);
+
+// Snapshot nos permite utilizar la base de datos en etiempo real de firestore
+function manejarSnapshot(snapshot) {
+
+}
+
   return (
     <>
       <h1 className="text-3xl font-light mb-4">Menu</h1>
